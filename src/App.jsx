fd475e137697fe6daa8ad80d2110c53729696273
Nathan Skiles <nateskiles94@gmail.com>
2023-10-02 16:18:31 -0700

    add state/hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Huey" species="Cat" breed="Tux" />
-    <Pet name="Dusty" species="Cat" breed="Ragdoll" />
-    <Pet name="Haley" species="Dog" breed="German Shepard" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
